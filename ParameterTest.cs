351fc0bf74c2d8fdd66675a51e2b4532e0f6a2b2
Martin Wohlauer <mwohlauer@posteo.de>
2021-03-23 18:33:32 +0100

    Now with a proper test case

---
+++ ParameterTest.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+
+public static class Efstats2 {
+    public static int Main(string[] args) {
+        ConsoleParameters.InitializeParameters("--",
+                                               new ParameterDefinition[] {
+                                                   new ParameterDefinition("test",
+                                                                           ParameterType.Boolean,
+                                                                           false),
+                                                   new ParameterDefinition("bier",
+                                                                           ParameterType.String,
+                                                                           true),
+                                               },
+                                               args);
+        Console.WriteLine("The following console values were provided: " + commaConcatStringList(args));
+        if (ConsoleParameters.getIsTainted()) {
+            List<string> missingParameters = ConsoleParameters.getMissingButRequiredParameterNames();
+            if (missingParameters.Count > 0) {
+                string missingParameterNames = commaConcatStringList(missingParameters);
+                Console.WriteLine("The following parameters are required but were not provided: " + missingParameterNames);
+            }
+            List<string> doubledParameters = ConsoleParameters.getDoubledParameterNames();
+            if (doubledParameters.Count > 0) {
+                string doubledParameterNames = commaConcatStringList(doubledParameters);
+                Console.WriteLine("The following parameters were provided at least twice by the user: " + doubledParameterNames);
+            }
+            List<string> unknownParameters = ConsoleParameters.getUnknownParameterNames();
+            if (unknownParameters.Count > 0) {
+                string unknownParametersNames = commaConcatStringList(unknownParameters);
+                Console.WriteLine("The following unknown parameters were provided: " + unknownParametersNames);
+            }
+        }
+        Parameter TestParameter = ConsoleParameters.getParameterByName("test");
+        bool asTest = TestParameter.getBoolValue();
+        if (asTest) {
+            Console.WriteLine("'test' was provided");
+        }
+        else {
+            Console.WriteLine("'test' was not provided");
+        }
+        //Console.WriteLine(ConsoleParameters.getStartCommand());
+
+        return 0;
+    }
+
+    private static string commaConcatStringList(List<string> strings) {
+        string output = null;
+        foreach (string element in strings) {
+            if (output == null) {
+                output = element;
+            }
+            else {
+                output += ", " + element;
+            }
+        }
+        return output;
+    }
+
+    private static string commaConcatStringList(string[] strings) {
+        string output = null;
+        foreach (string element in strings) {
+            if (output == null) {
+                output = element;
+            }
+            else {
+                output += ", " + element;
+            }
+        }
+        return output;
+    }
+}
--- makefile
+++ makefile
@@ -1,11 +1,11 @@
-all : efstats2.exe
+all : parametertest.exe
 ifeq ($(OS),Windows_NT)
-efstats2.exe : Efstats2.cs C:\\Windows\\Microsoft.NET\\Framework\\v4.0.30319\\csc.exe -out:efstats2.exe Efstats2.cs
+parametertest.exe : ParameterTest.cs C:\\Windows\\Microsoft.NET\\Framework\\v4.0.30319\\csc.exe -out:parametertest.exe ParameterTest.cs
 clean:
-	if exist efstats2.exe del masterserver.exe
+	if exist parametertest.exe del parametertest.exe
 else
-efstats2.exe : Efstats2.cs ConsoleParameters.cs ParameterDefinition.cs Parameter.cs
-	mcs -out:efstats2.exe Efstats2.cs ConsoleParameters.cs ParameterDefinition.cs Parameter.cs
+parametertest.exe : ParameterTest.cs ConsoleParameters.cs ParameterDefinition.cs Parameter.cs
+	mcs -out:parametertest.exe ParameterTest.cs ConsoleParameters.cs ParameterDefinition.cs Parameter.cs
 clean:
-	rm -f efstats2.exe
+	rm -f parametertest.exe
 endif
